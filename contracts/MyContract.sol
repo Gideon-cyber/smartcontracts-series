8a54c7139e2751ff49426ecffa074829a1a8ad9e
Gideon-cyber <gideonjohnson70@gmail.com>
2024-09-07 01:20:26 +0100

    update to inheritance in smartcontracts

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,7 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyContract {
+contract Ownable {
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
+
+contract SecretVault {
+    string secret;
+
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory){
+        return secret;
+    }
+}
+
+contract MyContract is Ownable {
     // State Variables vs Local Variables
     // Local variables are variables that exist within functions
 
@@ -107,5 +132,24 @@ contract MyContract {
     // function isOwner() public view returns(bool) {
     //     return (msg.sender == owner);
     // }
+
+    // // /// Inheritance
+    // // /// Factories (smart contract that creates other smart contract)
+    // // /// Interaction
+
+    // address secretVault;
+
+    // constructor(string memory _secret) {
+        
+    //     SecretVault _secretVault = new SecretVault(_secret);
+    //     secretVault = address(_secretVault); 
+    //     super;
+    // }
+
+    // function getSecret() public view onlyOwner returns(string memory){
+    //     return SecretVault(secretVault).getSecret();
+    // }
+
+    
 
 }
